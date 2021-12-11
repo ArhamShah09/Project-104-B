d93da32b6eaa0ef706901586e30c8126a037696e
ArhamShah09 <65812990+ArhamShah09@users.noreply.github.com>
2021-12-11 14:29:00 +0530

    Add files via upload

---
+++ Mean.py
@@ -0,0 +1,23 @@
+#Average
+
+import csv
+
+with open('Weight.csv', newline='') as f :
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+file_data.pop(0)
+new_data = []
+
+for i in range(len(file_data)):
+	n_number = file_data[i][2]
+	new_data.append(float(n_number))
+
+n = len(new_data)
+total = 0
+
+for x in new_data:
+	total += x
+mean = total/n
+
+print(mean)
